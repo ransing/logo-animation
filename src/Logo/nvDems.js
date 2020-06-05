25d27c13bcd6c5c5bf82fa522ded29055fc4b92c
viraj <virajransing@gmail.com>
2020-06-05 18:13:33 -0400

    first

---
+++ src/Logo/nvDems.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Nvdems from './nvDems.svg'
+
+export default function nvDems() {
+    return (
+        <div>
+            <img className="logo1" src={Nvdems}  />
+        </div>
+    )
+}
