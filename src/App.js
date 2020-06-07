2e212064511cc3001e586e6a858152c67efafcfe
viraj <virajransing@gmail.com>
2020-06-07 19:02:10 -0400

    added background route

--- src/App.js
+++ src/App.js
@@ -4,14 +4,26 @@ import './App.css';
 import NvDems from './Logo/nvDems';
 import NvVideo from './Logo/nvVideo'
 import { Route, Switch, withRouter, Link } from 'react-router-dom';
+import NavBar from './Navbar/navBar';
+import Background from './Background/background'
 
+const navlinks = [
+  { name: "Home", to: "/" },
+  { name: "video", to: "/video" },
+  { name: "Contact", to: "/contact" },
+  {name: "Background", to:"/background"}
+];
+
+const brand = { name: "peekaboo", to: "home" };
 
 function App() {
   return (
     <div className="App">
+      <NavBar brand={brand} links={navlinks} />
         <Switch>
           <Route exact path='/' component={NvDems}/>
           <Route exact path='/video' component={NvVideo}/>
+          <Route exact path='/background' component={Background}/>
         </Switch>
     </div>
   );
---
+++ src/Background/background.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import "./background.css";
+import One from '../video/one.mp4';
+
+export default function background() {
+    return (
+                    <div>
+                        <header>
+                {/* <video autoplay playsinline muted loop preload >
+                    <source src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/ocean-small.webm" />
+                <source src="http://thenewcode.com/assets/videos/ocean-small.mp4" />
+                </video> */}
+
+            <video className="videoOne" autoPlay muted loop preload >
+                <source src={One} type="video/mp4"/>
+            </video>
+
+
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 285 80" preserveAspectRatio="xMidYMid slice">
+                <defs>
+                <mask id="mask" x="0" y="0" width="100%" height="100%" >
+                <rect x="0" y="0" width="100%" height="100%" />
+                <text x="72"  y="50">NV DEMS</text>
+                    
+                </mask>
+            </defs>
+            <rect x="0" y="0" width="100%" height="100%" />
+            </svg>
+            </header>
+            
+        </div>
+    )
+}
